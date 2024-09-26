5f65401dd6d0068c2f325ff21e7091322d0bd886
Daniel24356 <igwedaniel128@gmail.com>
2024-09-26 16:43:43 -0700

    added about page

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import Navbar from './COMPONENTS/Navbar/Navbar'
 import ScrollTop from './components/ScrollTop'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Contact from './PAGES/Contact/Contact'
+import About from './PAGES/About/about'
 
 function App() {
 
   return (
     <>
+       {/* <Navbar/>
+       <About/>
+       <Footer/> */}
          <Router>
         <Navbar/>
         <ScrollTop/>
         <Routes>
         <Route path='/' index element={<Homepage/>}/>  
-        <Route path='/Contact' index element={<Contact/>}/>  
+        <Route path='/Contact' index element={<Contact/>}/> 
+        <Route path='/About' index element={<About/>}/> 
         </Routes>
         <Footer/>
       </Router>
